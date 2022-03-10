2f452e70da41958a0aba5c652711b0418e65fe99
I-M-F <matimu.f.ignatius@gmail.com>
2022-03-10 22:18:57 +0300

    Require Statement Solution commit

--- contracts/Access Control/AgreedPrice.sol
+++ contracts/Access Control/AgreedPrice.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
+
 contract AgreedPrice {
+    address public owner;
     uint256 public price;
 
     constructor(uint256 _price) {
+        owner = msg.sender;
         price = _price;
     }
 
     function updatePrice(uint256 _price) external {
+        require(msg.sender == owner, "Restricted Access");
         price = _price;
     }
 }
--- test/access-control-test.js
+++ test/access-control-test.js
@@ -2,24 +2,31 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Access Control", () => {
-    let deployer, attacker;
-  
-    beforeEach(async function () {
-      [deployer, attacker] = await ethers.getSigners();
-  
-      const AgreedPrice = await ethers.getContractFactory("AgreedPrice", deployer);
-      this.agreedPrice = await AgreedPrice.deploy(100);
+  let deployer, attacker;
+
+  beforeEach(async function () {
+    [deployer, attacker] = await ethers.getSigners();
+
+    const AgreedPrice = await ethers.getContractFactory("AgreedPrice", deployer);
+    this.agreedPrice = await AgreedPrice.deploy(100);
+  });
+
+  describe("AgreedPrice", () => {
+    it("Should set price at deployment", async function () {
+      expect(await this.agreedPrice.price()).to.eq(100);
     });
-  
-    describe("AgreedPrice", () => {
-      it("Should set price at deployment", async function () {
-        expect(await this.agreedPrice.price()).to.eq(100);
-      });
-  
-      it("Should be possible for anyone to change price", async function () {
-        await this.agreedPrice.updatePrice(1000);
-        
-        expect(await this.agreedPrice.price()).to.eq(1000);
-      });
+
+    it("Should set the deployer account as the owner at deployment", async function () {
+      expect(await this.agreedPrice.owner()).to.eq(deployer.address);
     });
+    it("Should be possible for the owner to change price", async function () {
+      await this.agreedPrice.updatePrice(1000);
+      expect(await this.agreedPrice.price()).to.eq(1000);
+    });
+    it("Should NOT be possible for other than the owner to change price", async function () {
+      await expect(this.agreedPrice.connect(attacker).updatePrice(1000)).to.be.revertedWith("Restricted Access");
+    });
+
+
+  });
 });
