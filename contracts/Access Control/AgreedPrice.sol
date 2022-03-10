7adf53bddb7dedcae6f6c90c5c2cc22306b2b98a
I-M-F <matimu.f.ignatius@gmail.com>
2022-03-10 22:59:39 +0300

    Community Vetted Ownable Solution commit

--- contracts/Access Control/AgreedPrice.sol
+++ contracts/Access Control/AgreedPrice.sol
@@ -1,25 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
-contract AgreedPrice {
-    address public owner;
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract AgreedPrice is Ownable {
     uint256 public price;
 
     constructor(uint256 _price) {
-        owner = msg.sender;
-        price = _price;
+       price = _price;
     }
 
-    modifier justOwner() {
-        require(msg.sender == owner, "Restricted Access");
-        _;
-    }
-
-    function changeOwner(address _newOwner) external justOwner {
-        owner = _newOwner;
-    }
-
-    function updatePrice(uint256 _price) external justOwner {
+    function updatePrice(uint256 _price) external onlyOwner {
         price = _price;
     }
 }
--- test/access-control-test.js
+++ test/access-control-test.js
@@ -24,22 +24,22 @@ describe("Access Control", () => {
       expect(await this.agreedPrice.price()).to.eq(1000);
     });
     it("Should NOT be possible for other than the owner to change price", async function () {
-      await expect(this.agreedPrice.connect(attacker).updatePrice(1000)).to.be.revertedWith("Restricted Access");
+      await expect(this.agreedPrice.connect(attacker).updatePrice(1000)).to.be.revertedWith("Ownable: caller is not the owner");
     });
 
     it("Should be possible for the owner to transfer ownership ", async function () {
-      await this.agreedPrice.changeOwner(user.address);
+      await this.agreedPrice.transferOwnership(user.address);
       expect(await this.agreedPrice.owner()).to.eq(user.address);
     });
     
     it("Should be possible for a new owner to call updatePrice", async function () {
-      await this.agreedPrice.changeOwner(user.address);
+      await this.agreedPrice.transferOwnership(user.address);
       await this.agreedPrice.connect(user).updatePrice(1000);
       expect(await this.agreedPrice.price()).to.eq(1000);
     });
     
     it("Should not be possible for other than the owner to transfer ownership", async function () {
-      await expect(this.agreedPrice.connect(attacker).changeOwner(attacker.address)).to.be.revertedWith("Restricted Access");
+      await expect(this.agreedPrice.connect(attacker).transferOwnership(attacker.address)).to.be.revertedWith("Ownable: caller is not the owner");
     });
 
   });
