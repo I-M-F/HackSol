6ec3afdd70776cafc2ce71612ce8b91bce9ab2bc
I-M-F <matimu.f.ignatius@gmail.com>
2022-03-12 12:25:39 +0300

    Reentrancy Solution commit

--- contracts/Reentrancy/SavingsAccountV2.sol
+++ contracts/Reentrancy/SavingsAccountV2.sol
@@ -3,27 +3,29 @@ pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/utils/Address.sol";
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
+contract SavingsAccountV2 is ReentrancyGuard{
+    using Address for address payable;
 
-contract SavingsAccountV2 {
-  using Address for address payable;
+    mapping(address => uint256) public balanceOf;
 
-  mapping(address => uint256) public balanceOf;
+    function deposit() external payable nonReentrant {
+        balanceOf[msg.sender] += msg.value;
+    }
 
-  function deposit() external payable {
-    balanceOf[msg.sender] += msg.value;
-  }
+    function withdraw() external nonReentrant {
+        require(balanceOf[msg.sender] > 0, "Nothing to Withdraw");
 
-  function withdraw() external {
-    console.log("");
-    console.log("ReentrancyVictim's balance: ", address(this).balance);
-    console.log("ReentrancyAttacker's balance: ", balanceOf[msg.sender]);
-    console.log("");
+        uint256 depositedAmount = balanceOf[msg.sender];
+        balanceOf[msg.sender] = 0;
 
-    uint256 depositedAmount = balanceOf[msg.sender];
+        console.log("");
+        console.log("ReentrancyVictim's balance: ", address(this).balance);
+        console.log("ReentrancyAttacker's balance: ", balanceOf[msg.sender]);
+        console.log("");
 
-    payable(msg.sender).sendValue(depositedAmount);
-
-    balanceOf[msg.sender] = 0;
-  }
+        payable(msg.sender).sendValue(depositedAmount);
+        balanceOf[msg.sender] = 0;
+    }
 }
